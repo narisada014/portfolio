a9f00f60bb9826dee476efeaa70ee07088edd700
narisada014 <nari.reishi@gmail.com>
2019-08-18 19:46:37 +0900

    implement upload to s3

--- app/pages/books/index.vue
+++ app/pages/books/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
     <h1>本のコレクション作成</h1>
+    <el-input
+      class="title"
+      type="input"
+      autosize
+      placeholder="本のタイトルを入力してください"
+      v-model="title"
+    >
+    </el-input>
+    <div style="margin: 20px 0;"></div>
+    <el-input
+      class="book-textarea"
+      type="textarea"
+      :autosize="{ minRows: 2, maxRows: 4}"
+      placeholder="本の概要・本同士の関連性を記入してください"
+      v-model="textarea">
+    </el-input>
+    <div style="margin: 20px 0;"></div>
     <el-upload
       class="avatar-uploader"
       drag
@@ -13,7 +30,7 @@
       <img v-if="imageUrl" :src="imageUrl" class="avatar">
       <i v-else class="el-icon-plus avatar-uploader-icon"></i>
     </el-upload>
-    <el-button size="mini" type="primary" @click="handleUpload">選択した画像をアップロードする</el-button>
+    <el-button size="mini" type="primary" @click="handleSave">本を登録する</el-button>
   </div>
 </template>
 
@@ -22,16 +39,25 @@
   export default {
     data() {
       return {
-        imageUrl: ''
+        imageUrl: '',
+        title: '',
+        textarea: '',
+        // vuexに移動
+        isSuccessResponse: false
       }
     },
     methods: {
-      handleAdd: function (file) {
+      handleAdd: async function (file) {
         this.imageUrl = URL.createObjectURL(file.raw);
-      },
-      handleUpload: async function() {
-        const signedUrl = await axios.get('http://localhost:3000/users/upload')
-        console.log(signedUrl)
+        const response = await axios.get('http://localhost:3000/users/upload')
+        axios.put(response.data.url, file.raw, {
+          headers: {
+            "Content-Type": file.raw.type
+          }
+        }).then(res => {
+          this.isSuccessResponse = true
+          console.log(res)
+        })
       },
       beforeAvatarUpload(file) {
         const isJPG = file.type === 'image/jpeg';
@@ -44,12 +70,25 @@
           this.$message.error('Avatar picture size can not exceed 2MB!');
         }
         return isJPG && isLt2M;
+      },
+      handleSave() {
+        console.log('hoge')
       }
     }
   }
 </script>
 
 <style>
+  .title {
+    width: 500px;
+    border-radius: 4px;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  }
+  .book-textarea {
+    width: 500px;
+    border-radius: 4px;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  }
   .avatar-uploader .el-upload {
     border: 1px dashed #d9d9d9;
     border-radius: 6px;
@@ -72,5 +111,8 @@
     width: 178px;
     height: 178px;
     display: block;
+  }
+  .el-upload-dragger {
+    width: 180px;
   }
 </style>
