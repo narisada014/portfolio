b06f8a45cdd0dd5b3228f5cf6dde2994662c9f05
narisada014 <nari.reishi@gmail.com>
2019-08-31 12:15:18 +0900

    prepare collection page

---
+++ app/pages/books/collections.vue
@@ -0,0 +1,27 @@
+<template>
+  collectionページ
+</template>
+
+<script>
+  import { Auth } from 'aws-amplify'
+  export default {
+    beforeCreate() {
+      let loggedIn = false
+      try {
+        Auth.currentUserInfo().then(user => {
+          loggedIn = Boolean(user)
+          if (!loggedIn) {
+            this.$router.push('/login')
+          }
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @media screen and (min-width: 767px) {
+  }
+</style>
